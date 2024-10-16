262683bc38b5957ce7a55c5ca6062d96b1e4ec3a
Rishi Bhachu <mr_bhachu@hotmail.com>
2024-10-16 20:33:37 +0100

    update node version

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,30 @@
+// import React from 'react'; dont need to declare for components from v17+
 import Navbar1 from '../components/Navbar'; // top nav
 import Home from '../components/Home'; // home page
 import Create from '../components/Create'; // create post page
-import BlogDetails from '../components/BlogDetails'; // dynamic blog post page
+import BlogDetails from '../components/BlogDetails'; // dynamic blog past page
 import NotFound from '../components/404'; // 404 page
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // updated for React Router v6
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // for React Router
 
+// root component
 function App() {
 
   return (    
     <Router>
+
       <div className="App">
         <Navbar1 />      
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/blogs/:id" element={<BlogDetails />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/"><Home /></Route>
+              <Route exact path="/create"><Create /></Route>
+              <Route exact path="/blogs/:id"><BlogDetails /></Route>
+              <Route path="*"><NotFound /></Route>
+            </Switch>
+          </div>
       </div>
+    
     </Router>
   );
 
